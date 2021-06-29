6aca4de55a933fa4ca6bad58945093b0c3851528
unknown <chevymanguiob@gmail.com>
2021-06-29 13:12:05 +0800

    fixed import column

--- app/Imports/StudentImport.php
+++ app/Imports/StudentImport.php
@@ -32,9 +32,6 @@ class StudentImport implements ToModel,WithHeadingRow,WithValidation
             "contact"  => $row["contact"],
             "facebook"  => $row["facebook"],
             "email"  => $row["email"],
-            "guardian_name"  => $row["guardian_name"],
-            "guardian_contact"  => $row["guardian_contact"],
-            "guardian_facebook"  => $row["guardian_facebook"],
             "student_avatar"  => $row["student_avatar"]
         ]);
     }
