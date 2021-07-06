b65642f1e2eadea8a80717302be716aad626b5ea
alasacelui <alasacelui@gmail.com>
2021-07-06 13:52:20 +0800

    added dynamic redirect after reset password;

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -213,6 +213,9 @@
 
 	{{--Monthly Sale--}}
 	<script>
+
+window.onload = function() {
+    
 		const my_chart = document.getElementById('chart_ms');
 
 		const chart_ms = new Chart(my_chart,{
@@ -285,7 +288,7 @@
             nextArrow: "<span title=\"Next month\">&raquo;</span>",
         });
     });
-
+}
 
 
 	</script>
