c3660c398ef5d6ca02d1cb0900218879b5a8b58d
alasacelui <alasacelui010@gmail.com>
2021-07-11 17:51:53 +0800

    Refactor javascript modules from cdn to designated file

--- resources/views/layouts/admindashboard.blade.php
+++ resources/views/layouts/admindashboard.blade.php
@@ -364,33 +364,34 @@
     {{--Scripts--}}
     @routes 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdn.rawgit.com/harvesthq/chosen/gh-pages/chosen.jquery.min.js"></script>
 
 	{{-- <script src="js/jq.js"></script> --}}
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 	<script src="{{ asset('admin/js/app.js') }}"></script>
 
-	{{--DT--}}
-    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
-	<script src="https://cdn.datatables.net/rowreorder/1.2.7/js/dataTables.rowReorder.min.js"></script>
-	<script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap5.min.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.bootstrap5.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
-	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.colVis.min.js"></script>
+   	{{--DT--}}
+	   <script src="{{ asset('dist/jquery.dataTables.min.js') }}"></script>
+	   <script src="{{ asset('dist/dataTables.bootstrap5.min.js') }}"></script>
+	   <script src="{{ asset('dist/dataTables.rowReorder.min.js') }}"></script>
+	   <script src="{{ asset('dist/dataTables.responsive.min.js') }}"></script>
+	   <script src="{{ asset('dist/dataTables.buttons.min.js') }}"></script>
+	   <script src="{{ asset('dist/buttons.bootstrap5.min.js') }}"></script>
+	   <script src="{{ asset('dist/jszip.min.js') }}"></script>
+	   <script src="{{ asset('dist/pdfmake.min.js') }}"></script>
+	   <script src="{{ asset('dist/vfs_fonts.js') }}"></script>
+	   <script src="{{ asset('dist/buttons.html5.min.js') }}"></script>
+	   <script src="{{ asset('dist/buttons.print.min.js') }}"></script>
+	   <script src="{{ asset('dist/buttons.colVis.min.js') }}"></script>
 
 	{{--SA--}}
-	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	   <script src="{{ asset('dist/alert.js') }}"></script>
+
 	{{--Toastr--}}
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+	   <script src="{{ asset('dist/toastr.min.js') }}"></script>
 
 	{{--Tags--}}
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js" integrity="sha512-9UR1ynHntZdqHnwXKTaOm1s6V9fExqejKvg5XMawEMToW4sSw+3jtLrYfZPijvnwnnE8Uol1O9BcAskoxgec+g==" crossorigin="anonymous"></script>
+	<script src="{{ asset('dist/bs_tagsinput.min.js') }}"></script>
+
 
 	{{--E-one--}}
     <script src="{{ asset('js/eone.min.js') }}"></script>
@@ -398,7 +399,7 @@
 
 	{{--Scripts--}}
     <script src="{{ asset('js/preview.js') }}"></script>
-    <script src="{{ asset('js/bundle.js') }}"></script>
+    <script src="{{ asset('js/ajax.js') }}"></script>
     <script src="{{ asset('js/script.js') }}"></script>
     {{-- <script src="{{ asset('js/statistic.js') }}"></script> --}}
      
